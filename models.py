f0b42aa341534a298c890162231e4dfb6df1aca9
Dale Floer <dalefloer@gmail.com>
2020-02-08 20:58:22 -0800

    Updated models. Some fields not implemented yet.

--- models.py
+++ models.py
@@ -15,12 +15,16 @@ class Country(Base):
     country_code = Column(Text, primary_key=True, index=True)
     country = Column(Text, nullable=False, unique=True)
 
+    locations = relationship("Location", backref="location_country")
+
 
 class County(Base):
     __tablename__ = 'county'
 
-    county = Column(Text, primary_key=True, index=True)
-    county_code = Column(Text, nullable=False, unique=True)
+    county = Column(Text, nullable=False, index=True)
+    county_code = Column(Text, primary_key=True, unique=True)
+
+    locations = relationship("Location", backref="location_county")
 
 
 class Locality(Base):
@@ -30,28 +34,36 @@ class Locality(Base):
     locality_id = Column(Integer, primary_key=True)
     locality_type = Column(String(2), nullable=False)
 
+    locations = relationship("Location", backref="location_locality")
+
 
 class Observer(Base):
     __tablename__ = 'observer'
 
     observer_id = Column(Integer, primary_key=True)
-    first_name = Column(Text, nullable=False)
-    last_name = Column(Text, nullable=False)
+
+    observations = relationship("Observation", backref="observations_observer")
 
 
 class Species(Base):
     __tablename__ = 'species'
 
-    common_name = Column(Text, nullable=False)
+    common_name = Column(Text, nullable=False, index=True)
     scientific_name = Column(Text, primary_key=True, index=True)
-    taxonomic_order = Column(Numeric(20, 10), nullable=False)
+    taxonomic_order = Column(Integer, index=True)
+    species_code = Column(String(8), index=True)
+
+    subspecies_of = relationship("SubSpecies", backref="subspecies_scientific_name")
+    observations = relationship("Observation", backref="observatoin_species")
 
 
 class StateProvince(Base):
     __tablename__ = 'stateprovince'
 
-    state_province = Column(Text, primary_key=True, index=True)
-    state_code = Column(Text, nullable=False, unique=True)
+    state_province = Column(Text, nullable=False, index=True)
+    state_code = Column(Text, primary_key=True, unique=True)
+
+    locations = relationship("Location", backref="location_state_province")
 
 
 class Location(Base):
@@ -60,17 +72,19 @@ class Location(Base):
     id = Column(Integer, primary_key=True)
     coords = Column(Geometry(geometry_type='POINT', srid=4326), nullable=False, index=True)
     country_id = Column(ForeignKey('country.country_code', deferrable=True, initially='DEFERRED'), nullable=False, index=True)
-    county_id = Column(ForeignKey('county.county', deferrable=True, initially='DEFERRED'), nullable=False, index=True)
+    county_id = Column(ForeignKey('county.county_code', deferrable=True, initially='DEFERRED'), nullable=False, index=True)
     locality_id = Column(ForeignKey('locality.locality_id', deferrable=True, initially='DEFERRED'), nullable=False, index=True)
-    state_province_id = Column(ForeignKey('stateprovince.state_province', deferrable=True, initially='DEFERRED'), nullable=False, index=True)
+    state_province_id = Column(ForeignKey('stateprovince.state_code', deferrable=True, initially='DEFERRED'), nullable=False, index=True)
 
-    country = relationship('Country')
-    county = relationship('County')
-    locality = relationship('Locality')
-    state_province = relationship('StateProvince')
+    # country = relationship('Country')
+    # county = relationship('County')
+    # locality = relationship('Locality')
+    # state_province = relationship('StateProvince')
+
+    locations = relationship("Checklist", backref="location_checklist")
 
 
-class subspecies(Base):
+class SubSpecies(Base):
     __tablename__ = 'subspecies'
 
     common_name = Column(Text, nullable=False)
@@ -78,8 +92,10 @@ class subspecies(Base):
     taxonomic_order = Column(Numeric(20, 10), nullable=False)
     category = Column(Integer)
     parent_species_id = Column(ForeignKey('species.scientific_name', deferrable=True, initially='DEFERRED'), index=True)
+    subspecies_code = Column(String(8), index=True)
 
-    parent_species = relationship('Species')
+    # parent_species = relationship('Species')
+    observations = relationship("Observation", backref="observation_ubspecies")
 
 
 class Checklist(Base):
@@ -99,8 +115,10 @@ class Checklist(Base):
     reason = Column(Text, nullable=False)
     protocol = Column(String(2))
     location_id = Column(ForeignKey('location.id', deferrable=True, initially='DEFERRED'), nullable=False, index=True)
+    project_code = Column(Text, nullable=False)
 
-    location = relationship('Location')
+    # location = relationship('Location')
+    observations = relationship("Observation", backref="checklist_observation")
 
 
 class Observation(Base):
@@ -111,7 +129,6 @@ class Observation(Base):
     is_x = Column(Boolean, nullable=False)
     age_sex = Column(Text, nullable=False)
     species_comments = Column(Text)
-    breeding_atlas_code = Column(String(2))
     date_last_edit = Column(DateTime(True))
     has_media = Column(Boolean, nullable=False)
     checklist_id = Column(ForeignKey('checklist.checklist', deferrable=True, initially='DEFERRED'), nullable=False, index=True)
@@ -119,7 +136,11 @@ class Observation(Base):
     species_id = Column(ForeignKey('species.scientific_name', deferrable=True, initially='DEFERRED'), index=True)
     subspecies_id = Column(ForeignKey('subspecies.scientific_name', deferrable=True, initially='DEFERRED'), index=True)
 
-    checklist = relationship('Checklist')
-    observer = relationship('Observer')
-    species = relationship('Species')
-    subspecies = relationship('SubSpecies')
+    # checklist = relationship('Checklist')
+    # observer = relationship('Observer')
+    # species = relationship('Species')
+    # subspecies = relationship('SubSpecies')
+
+
+# Not implemented fields from the data (yet):
+# IBA CODE, BCR CODE, USFWS CODE, ATLAS BLOCK, BREEDING BIRD ATLAS CODE, BREEDING BIRD ATLAS CATEGORY
